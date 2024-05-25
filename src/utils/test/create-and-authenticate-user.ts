921a1440b5a27ce6759f305621ff6ecc2a28e10b
viniibp <viniibp@gmail.com>
2024-05-25 16:10:45 -0300

    github actions

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,7 +1,21 @@
+import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs";
 import { FastifyInstance } from "fastify";
 import request from "supertest";
 
-export async function createAndAuthenticateUser(app:FastifyInstance) {
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+  isAdmin = false
+) {
+  const user = await prisma.user.create({
+    data: {
+      name: "User test",
+      email: "test@example.com.br",
+      password_hash: await hash("123456", 6),
+      role: isAdmin ? "ADMIN" : "MEMBER",
+    },
+  });
+
   await request(app.server).post("/users").send({
     name: "User test",
     email: "test@example.com.br",
@@ -15,5 +29,5 @@ export async function createAndAuthenticateUser(app:FastifyInstance) {
 
   const { token } = authResponse.body;
 
-  return {token};
+  return { token };
 }
