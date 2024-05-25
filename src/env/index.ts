4737ac9fffce5ea9a85a6fa19d8cac4aa96b8691
viniibp <viniibp@gmail.com>
2024-05-24 21:05:28 -0300

    auth with fastif-jwt token

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 
 const envSchema = z.object({
   NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
+  JWT_SECRET: z.string(),
   PORT: z.coerce.number().default(3333),
 });
 
@@ -11,7 +12,7 @@ const _env = envSchema.safeParse(process.env);
 if (_env.success === false) {
   console.error("❌ Invalid enviroment variables", _env.error.format());
 
-  throw new Error('Invalid enviroment variables.');
+  throw new Error("Invalid enviroment variables.");
 }
 
 export const env = _env.data;
--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -4,7 +4,10 @@ import { z } from "zod";
 import { InvalidCredentialsError } from "@/services/errors/invalid-credentials-error";
 import { makeAuthenticateService } from "@/services/factories/make-authenticate-service";
 
-export async function authenticate(request: FastifyRequest, reply: FastifyReply) {
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
   const authenticateBodySchema = z.object({
     email: z.string().email(),
     password: z.string().min(6),
@@ -13,17 +16,25 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
   const { email, password } = authenticateBodySchema.parse(request.body);
 
   try {
-
     const authenticateService = makeAuthenticateService();
 
-    await authenticateService.execute({ email, password });
+    const { user } = await authenticateService.execute({ email, password });
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      }
+    );
+
+    return reply.status(200).send({ token });
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: error.message });
     }
-   
+
     throw error;
   }
-
-  return reply.status(200).send();
 }
